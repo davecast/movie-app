b8ca6ba6ab095919a64618f74f2a7ad5d92221d3
David Castillo <davecas26@gmail.com>
2025-08-07 22:06:34 -0400

    Start base of project movie example

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+import './global.css'
+
+const App = () => {
+  return (
+    <View className='flex-1 items-center justify-center' >
+      <Text className='text-red-500 text-2xl font-bold'>App</Text>
+    </View>
+  )
+}
+
+export default App

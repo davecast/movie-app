9ff82cba272c5ef16684a4b4bd8a1ab771ab0e52
David Castillo <davecas26@gmail.com>
2025-08-08 02:15:49 -0400

    Add all calls for anothers type movies

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,15 +1,25 @@
 import MainSlideshot from '@/components/main-slideshot';
+import { MovieHorizontalList } from '@/components/movies';
 import { useMovies } from '@/hooks/useMovies';
 import React from 'react';
-import { ActivityIndicator, Text, View } from 'react-native';
+import { ActivityIndicator, ScrollView, Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const HomeScreen = () => {
   const safeArea = useSafeAreaInsets();
-  const { moviesQuery } = useMovies();
-  const { data: movies, isLoading, error } = moviesQuery;
+  const { 
+    nowPlayingQuery, 
+    popularMoviesQuery, 
+    topRatedMoviesQuery, 
+    upcomingMoviesQuery 
+  } = useMovies();
 
-  if (isLoading) {
+  const { data: moviesNowPlaying, isLoading: isLoadingNowPlaying, error: errorNowPlaying } = nowPlayingQuery;
+  const { data: moviesPopular, isLoading: isLoadingPopular, error: errorPopular } = popularMoviesQuery;
+  const { data: moviesTopRated, isLoading: isLoadingTopRated, error: errorTopRated } = topRatedMoviesQuery;
+  const { data: moviesUpcoming, isLoading: isLoadingUpcoming, error: errorUpcoming } = upcomingMoviesQuery;
+
+  if (isLoadingNowPlaying || isLoadingPopular || isLoadingTopRated || isLoadingUpcoming) {
     return (
       <View className='flex-1 items-center justify-center'>
         <ActivityIndicator size="large" color="#ef4444" />
@@ -18,22 +28,57 @@ const HomeScreen = () => {
     );
   }
 
-  if (error) {
+  if (errorNowPlaying || errorPopular || errorTopRated || errorUpcoming) {
     return (
       <View className='flex-1 items-center justify-center'>
         <Text className='text-red-500 text-xl font-bold'>Error</Text>
         <Text className='text-gray-600 mt-2 text-center px-4'>
-          {error instanceof Error ? error.message : 'Error loading movies'}
+          {errorNowPlaying instanceof Error ? errorNowPlaying.message : 'Error loading movies'}
+          {errorPopular instanceof Error ? errorPopular.message : 'Error loading movies'}
+          {errorTopRated instanceof Error ? errorTopRated.message : 'Error loading movies'}
+          {errorUpcoming instanceof Error ? errorUpcoming.message : 'Error loading movies'}
         </Text>
       </View>
     );
   }
 
   return (
-    <View className='flex-1 bg-white' style={{ paddingTop: safeArea.top }}>
-      <Text className='text-red-500 text-2xl font-bold'>Movie App</Text>
-      <MainSlideshot movies={movies || []} />
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerClassName='pb-20'
+    >
+      <View className='flex-1' style={{ paddingTop: safeArea.top }}>
+      {/* Now playing movies */}
+      <View className='pt-6'>
+        <Text className='text-gray-600 px-4 text-2xl font-bold'>Movies App</Text>
+        <MainSlideshot movies={moviesNowPlaying || []} />
+      </View>
+
+      {/* Popular movies */}
+      <View className='pt-6'>
+        <MovieHorizontalList 
+          movies={moviesPopular || []} 
+          title='Popular movies'
+        />
+      </View>
+
+      {/* Top rated movies */}
+      <View className='pt-6'>
+        <MovieHorizontalList 
+          movies={moviesTopRated || []} 
+          title='Top rated movies'
+        />
+      </View>
+
+      {/* Upcoming movies */}
+      <View className='pt-6'>
+        <MovieHorizontalList 
+          movies={moviesUpcoming || []} 
+          title='Upcoming movies'
+        />
+      </View>
     </View>
+    </ScrollView>
   )
 }
 

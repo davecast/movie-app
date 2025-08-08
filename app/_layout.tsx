2988413d465e9559661e5fb57821cf689ed38598
David Castillo <davecas26@gmail.com>
2025-08-08 02:15:23 -0400

    Disbled warn for carousel, Add gesture root view, and set color for navbar android

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,18 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useFonts } from 'expo-font';
+import * as NavigationBar from 'expo-navigation-bar';
 import { Stack } from 'expo-router';
-import 'react-native-reanimated';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Platform } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { configureReanimatedLogger } from 'react-native-reanimated';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 import './global.css';
+
+// Configure Reanimated logger
+configureReanimatedLogger({ strict: false });
 
 const queryClient = new QueryClient()
 
@@ -14,23 +22,34 @@ export default function RootLayout() {
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
+  // Configure navigation bar for Android
+  React.useEffect(() => {
+    if (Platform.OS === 'android') {
+      NavigationBar.setStyle('light');
+    }
+  }, []);
+
   if (!loaded) {
     // Async font loading only occurs in development.
     return null;
   }
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <SafeAreaProvider>
-        <SafeAreaView className='flex-1' edges={['left', 'right']}>
-          <Stack 
-            screenOptions={{
-              headerShown: false,
-              headerShadowVisible: false,
-            }}
-          />
-        </SafeAreaView>
-      </SafeAreaProvider>
-    </QueryClientProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+     
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaProvider>
+          <SafeAreaView className='flex-1' edges={['left', 'right']}>
+            <Stack 
+              screenOptions={{
+                headerShown: false,
+                headerShadowVisible: false,
+              }}
+              />
+              <StatusBar style="dark" />
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </QueryClientProvider>
+    </GestureHandlerRootView>
   );
 }

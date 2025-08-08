a97592398a64004686071c13987c37d50aa2d0a0
David Castillo <davecas26@gmail.com>
2025-08-08 02:55:17 -0400

    Implement useInfiniteQuery from tanstack

--- hooks/useMovies.tsx
+++ hooks/useMovies.tsx
@@ -1,5 +1,5 @@
 import { getNowPlaying, getPopularMovies, getTopRatedMovies, getUpcomingMovies } from "@/core/services/movies";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 
 export const useMovies = () => {
   
@@ -9,22 +9,61 @@ export const useMovies = () => {
     staleTime: 1000 * 60 * 60 * 24, // 24 hours
   })
 
-  const popularMoviesQuery = useQuery({
+  // const popularMoviesQuery = useQuery({
+  //   queryKey: ['movies', 'popular'],
+  //   queryFn: () => getPopularMovies({ page: 1 }),
+  //   staleTime: 1000 * 60 * 60 * 24, // 24 hours
+  // })
+
+  // const topRatedMoviesQuery = useQuery({
+  //   queryKey: ['movies', 'top-rated'],
+  //   queryFn: () => getTopRatedMovies({ page: 1 }),
+  //   staleTime: 1000 * 60 * 60 * 24, // 24 hours
+  // })
+
+  // const upcomingMoviesQuery = useQuery({
+  //   queryKey: ['movies', 'upcoming'],
+  //   queryFn: () => getUpcomingMovies({ page: 1 }),
+  //   staleTime: 1000 * 60 * 60 * 24, // 24 hours
+  // })
+
+  const popularMoviesQuery = useInfiniteQuery({
+    initialPageParam: 1,
     queryKey: ['movies', 'popular'],
-    queryFn: getPopularMovies,
+    queryFn: ({ pageParam = 1 }) => {
+      return getPopularMovies({ page: pageParam })
+    },
     staleTime: 1000 * 60 * 60 * 24, // 24 hours
+    getNextPageParam: (lastPage, pages) => {
+      if (lastPage.length === 0) return undefined
+      return pages.length + 1
+    }
   })
 
-  const topRatedMoviesQuery = useQuery({
+  const topRatedMoviesQuery = useInfiniteQuery({
+    initialPageParam: 1,
     queryKey: ['movies', 'top-rated'],
-    queryFn: getTopRatedMovies,
+    queryFn: ({ pageParam = 1 }) => {
+      return getTopRatedMovies({ page: pageParam })
+    },
     staleTime: 1000 * 60 * 60 * 24, // 24 hours
+    getNextPageParam: (lastPage, pages) => {
+      if (lastPage.length === 0) return undefined
+      return pages.length + 1
+    }
   })
 
-  const upcomingMoviesQuery = useQuery({
+  const upcomingMoviesQuery = useInfiniteQuery({
+    initialPageParam: 1,
     queryKey: ['movies', 'upcoming'],
-    queryFn: getUpcomingMovies,
+    queryFn: ({ pageParam = 1 }) => {
+      return getUpcomingMovies({ page: pageParam })
+    },
     staleTime: 1000 * 60 * 60 * 24, // 24 hours
+    getNextPageParam: (lastPage, pages) => {
+      if (lastPage.length === 0) return undefined
+      return pages.length + 1
+    }
   })
 
   return {

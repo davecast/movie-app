1673a96e589c3111a23f09fb3acd7d0747563ac0
David Castillo <davecas26@gmail.com>
2025-08-08 02:16:19 -0400

    Add moti animation for show carousel

--- components/main-slideshot/main-slideshot.tsx
+++ components/main-slideshot/main-slideshot.tsx
@@ -1,6 +1,8 @@
+import { MoviePoster } from '@/components/movies';
 import { Movie } from '@/types/movies';
+import { AnimatePresence, View as ViewMoti } from 'moti';
 import React, { useRef } from 'react';
-import { Text, useWindowDimensions, View } from 'react-native';
+import { useWindowDimensions } from 'react-native';
 import Carousel, { ICarouselInstance } from "react-native-reanimated-carousel";
 
 interface MainSlideshotProps {
@@ -12,28 +14,43 @@ const MainSlideshot = ({ movies }: MainSlideshotProps) => {
   const width = useWindowDimensions().width;
 
   return (
-    <View className='h-[250px] w-full'>
-      <Carousel 
-        ref={ref}
-        data={movies}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
-        width={200}
-        height={350}
-        style={{
-          width: width,
-          height: 350,
-          justifyContent: 'center',
-          alignItems: 'center',
+    <AnimatePresence>
+      <ViewMoti
+        className='h-[250px] w-full'
+        from={{
+          opacity: 0,
         }}
-        mode='parallax'
-        modeConfig={{
-          parallaxScrollingScale: 0.9,
-          parallaxScrollingOffset: 50,
+        animate={{
+          opacity: 1,
         }}
-        defaultIndex={1}
-      />
-    </View>
+        transition={{
+          type: 'timing',
+          duration: 600,
+          delay: 500,
+        }}
+      >
+        <Carousel 
+          ref={ref}
+          data={movies}
+          renderItem={({ item }) => <MoviePoster id={item.id} poster={item.poster} />}
+          width={200}
+          height={350}
+          style={{
+            width: width,
+            height: 350,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          mode='parallax'
+          modeConfig={{
+            parallaxScrollingScale: 0.9,
+            parallaxScrollingOffset: 50,
+          }}
+          defaultIndex={1}
+        />
+      </ViewMoti>
+    </AnimatePresence>
   )
-}
+};
 
 export default MainSlideshot

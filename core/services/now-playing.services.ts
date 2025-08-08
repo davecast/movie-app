a8da7a2ffba141108b24071afb4b1466695429b0
David Castillo <davecas26@gmail.com>
2025-08-07 23:36:53 -0400

    Implement mapper to change response data structure

--- core/services/now-playing.services.ts
+++ core/services/now-playing.services.ts
@@ -1,11 +1,13 @@
 import { axios_client } from "@/core/api/axios.api";
+import { MovieMapper } from "@/core/utils/mappers";
 import { Movie, MoviesResponse } from "@/types/movies";
 
 export const getNowPlaying = async (): Promise<Movie[]> => {
   try {
     const { data } = await axios_client.get<MoviesResponse>('/now_playing');
-    return data.results || [];
-  } catch (error) {
+    const movies = data.results.map(MovieMapper.fromMovieResponseToMovie);
+    return movies
+  } catch (error) { 
     console.error('API Error:', error);
     throw 'Cannot load now playing movies';
   }
